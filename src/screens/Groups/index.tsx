6915f1899d50b50969feeca4b06ad74d348fb960
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-01-30 17:38:30 -0300

    feat: :sparkles: new component groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
+import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native'
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
+
+import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['discord da rocketseat', 'família'])
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Header showBackButton />
+
+      <Highlight title='Turmas' subtitle='jogue com a sua turma' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        )}
+      />
+      <Button
+        title='Criar nova turma'
+      />
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
