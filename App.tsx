df4ce54722523ade8132588ca722fc1a1f9e0f4d
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-01-30 17:23:19 -0300

    added ThemeProvider, StatusBar and Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
-import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+// import { NewGroup } from '@screens/NewGroup';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
+import theme from './src/theme';
+
+// import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players'
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Players /> : <Loading />}
+    </ThemeProvider>
   );
 }
