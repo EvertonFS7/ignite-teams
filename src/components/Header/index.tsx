7d8675b7fd21944e811acad838ca5c3cad6c3b65
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-02-02 13:31:44 -0300

    feat: :sparkles: adding navigate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import logoImg from '@assets/logo.png';
+import { useNavigation } from '@react-navigation/native';
 
 import { BackButton, BackIcon, Container, Logo } from './styles';
 
@@ -7,11 +8,18 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+    
+    const { navigate } = useNavigation()
+
+    function handleGoBack() {
+        navigate('groups')
+    }
+
     return (
         <Container>
             {
                 showBackButton && (
-                    <BackButton>
+                    <BackButton onPress={handleGoBack}>
                         <BackIcon />
                     </BackButton>
                 )
