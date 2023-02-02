a95d17c54b98299f9b038536dec0bb4aaedd9e80
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-02-02 13:26:49 -0300

    adding app routes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-// import { NewGroup } from '@screens/NewGroup';
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 
+import { Routes } from './src/routes';
 import theme from './src/theme';
 
-// import { Groups } from '@screens/Groups';
-import { Players } from '@screens/Players'
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
@@ -18,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
